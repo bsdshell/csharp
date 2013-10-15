b7bdb04dc4cac3b7a928e79baaac1d6b2ab2e18a
U-VOICEBOX\arons <arons@VBTD926-AS.voiceboxtechnologies.com>
2013-10-15 13:40:52 -0700

    XElement example

---
+++ XElementExample/XElementExample/XElementExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XElementExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement sitemap = XElement.Load("http://www.dotnetperls.com/sitemap.xml");
+
+            // ... XNames.
+            XName url = XName.Get("url", "http://www.sitemaps.org/schemas/sitemap/0.9");
+            XName loc = XName.Get("loc", "http://www.sitemaps.org/schemas/sitemap/0.9");
+
+            // ... Loop over url elements.
+            // ... Then access each loc element.
+            foreach (var urlElement in sitemap.Elements(url))
+            {
+                var locElement = urlElement.Element(loc);
+                Console.WriteLine(locElement.Value);
+            }
+
+            // ... Display count.
+            Console.WriteLine("Count: {0}", sitemap.Elements(url).Count());
+            Console.Read();
+        }
+    }
+}
