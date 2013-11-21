32bb73c8ba098a06196c851abb4e71e61512955c
U-VOICEBOX\arons <arons@VBTD926-AS.voiceboxtechnologies.com>
2013-11-21 11:24:53 -0800

    stop watch in c#

---
+++ StopWatchApp/StopWatchApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace StopWatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> stringList = new List<String>();
+            for (int i = 0; i < 1000000; i++)
+            {
+                stringList.Add(i + "");
+            }
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            foreach (String str in stringList)
+            {
+                Console.WriteLine(str);
+            }
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+
+            Console.WriteLine("RunTime " + elapsedTime);
+            Console.ReadLine();
+        }
+    }
+}
