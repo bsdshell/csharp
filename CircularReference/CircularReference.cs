ced869a93e95d2cace92a69d71e1836fdd34deea
bsdshell <arons@VBTD926-AS.voiceboxtechnologies.com>
2014-01-08 15:57:18 -0800

    C# Circular Reference

---
+++ CircularReference/CircularReference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircularReference
+{
+    public class MyClassA
+    {
+        MyClassB b;
+        public MyClassA(MyClassB b)
+        {
+            this.b = b;
+        }
+    }
+    public class MyClassB
+    {
+        MyClassA a;
+        public MyClassB()
+        {
+            a = new MyClassA(this);
+        }
+    }
+
+    class CircularReference
+    {
+        static void Main(string[] args)
+        {
+            MyClassB b = new MyClassB();
+            System.Console.WriteLine("MyClassA and MyClassB are connected(Circular Reference)");
+            System.Console.ReadLine();
+        }
+    }
+}
