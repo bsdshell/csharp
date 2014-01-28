4bc47d352de4a58ffb55061f5d983cebf624516b
bsdshell <arons@VBTD926-AS.voiceboxtechnologies.com>
2014-01-28 13:44:21 -0800

    Add DeploymentItem example

---
+++ DeploymentItemTest/TestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        //Copy sourceDir contest to destinationDir
+        [DeploymentItem("sourceDir", "destinationDir")]
+        public void TestMethod1()
+        {
+            string basePath = Path.Combine(TestContext.TestRunDirectory, "Out");
+            string fullPath = basePath + @"\destinationDir\baseText.txt";
+        }
+    }
+}
