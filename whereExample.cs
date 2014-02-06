af243ad7c535c327c83da6c1ca5b87835bc7941b
bsdshell <arons@VBTD926-AS.voiceboxtechnologies.com>
2014-02-06 10:34:27 -0800

    where example

---
+++ whereExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace whereExample
+{
+    public class MyClass<T> where T : IComparable, new()
+    {
+        //The following line is not possible without new() constraint
+        T item = new T();
+    }
+    class Program
+    {
+        static void Main(string[]  args)
+        {
+            System.Console.WriteLine("where constraints generic type");
+            System.Console.ReadLine();
+        }
+    }
+}
