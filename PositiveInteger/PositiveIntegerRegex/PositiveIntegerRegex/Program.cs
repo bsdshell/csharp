ccc720de497f7d7de9094fb774baf62e8256fea9
bsdshell <du@gmail.com>
2014-05-22 18:59:25 -0700

    add

---
+++ PositiveInteger/PositiveIntegerRegex/PositiveIntegerRegex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PositiveIntegerRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array =
+            {
+                "123",
+                "+123",
+                "0",
+                "0013"
+            };
+            string pattern = @"^[1-9]\d*$|^[+][1-9]\d*$";
+            Regex regex = new Regex(pattern, RegexOptions.Compiled);
+            foreach (string input in array)
+            {
+                Match match = regex.Match(input);
+                if (match.Success)
+                {
+                    Console.WriteLine("{0} = {1}", input, match.Success);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
