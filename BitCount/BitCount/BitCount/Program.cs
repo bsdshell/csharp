e3f54bebbfe7707a36726f14647456d8b2c9781e
bsdshell <du@gmail.com>
2014-06-25 14:44:32 -0700

    bit count

---
+++ BitCount/BitCount/BitCount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BitCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UInt32 n = 179;
+            long count = countBit(n);
+            System.Console.WriteLine("n={0}", count);
+            System.Console.ReadLine();
+        }
+
+        static long countBit(long n)
+        {
+            UInt32 c;
+            for (c = 0; n != 0; c++)
+            {
+                n &= n - 1;
+            }
+            return c;
+        }
+    }
+}
