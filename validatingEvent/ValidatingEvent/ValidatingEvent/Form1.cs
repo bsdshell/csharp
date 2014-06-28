de7e5c4e646ca26d8e51ba2aec53bca6ee84471e
bsdshell <du@gmail.com>
2014-06-27 17:15:31 -0700

    Validating Event

---
+++ validatingEvent/ValidatingEvent/ValidatingEvent/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ValidatingEvent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Event will be trigger when focus leaves the control(e.g TextBox)
+            this.textBox1.Validating += ValidationRule;
+        }
+        private void ValidationRule(object sender, EventArgs e)
+        {
+            MessageBox.Show("Validating me!", "Validating", MessageBoxButtons.YesNo);
+        }
+    }
+}
