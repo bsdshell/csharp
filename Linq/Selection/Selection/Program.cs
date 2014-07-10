faa94833ecccc0b17e2f17ac1b4253b362d96e2c
bsdshell <du@gmail.com>
2014-07-10 11:53:02 -0700

    Selection in Linq

---
+++ Linq/Selection/Selection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Selection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[]{1, 2, 4, 7, 9};
+
+            var source = number.Select(ele => new Num {num = ele*10}).ToList();
+            foreach (var s in source)
+            {
+                System.Console.WriteLine("s={0}", s.num);
+            }
+            System.Console.ReadLine();
+        }
+    }
+
+    public class Num
+    {
+        public int num;
+        public Num()
+        {}
+    }
+}
