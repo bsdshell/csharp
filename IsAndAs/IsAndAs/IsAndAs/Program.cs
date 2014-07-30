0f7c9f3c34a549ca654190848657eabd5a201298
bsdshell <du@gmail.com>
2014-07-30 16:29:12 -0700

    is and as duplicating cast operation

---
+++ IsAndAs/IsAndAs/IsAndAs/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IsAndAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> list = new List<object>();
+            list.Add(new DerivedClass());
+            list.Add(new DerivedClass());
+            list.Add(new CrazyClass());
+            BadCast(list);
+            GoodCast(list);
+            Console.ReadLine();
+        }
+
+        public static void BadCast(List<object> list)
+        {
+            foreach (Object obj in list)
+            {
+                if (obj is BaseClass)
+                {
+                    DerivedClass derivedClass = obj as DerivedClass;
+                    Console.WriteLine("duplicating cast operation with [is] and [as]");
+                }
+            }
+        }
+
+        public static void GoodCast(List<object> list)
+        {
+            foreach (Object obj in list)
+            {
+                DerivedClass derivedClass = obj as DerivedClass;
+                if (derivedClass != null)
+                {
+                    System.Console.WriteLine("Good cast with as");
+                }
+            }
+        }
+    }
+
+    class BaseClass{}
+    class DerivedClass : BaseClass{}
+    class CrazyClass{}
+}
